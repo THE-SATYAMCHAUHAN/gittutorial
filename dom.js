69f1e8d8e38d94e922fd64cee144171f9c87dd1c
THE-SATYAMCHAUHAN <110241083+THE-SATYAMCHAUHAN@users.noreply.github.com>
2023-09-27 23:58:47 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,11 +1,11 @@
 
-var itemall=document.getElementById('items');
-console.log(itemall) 
-itemall.style.fontWeight='bold'
-var items=document.getElementsByClassName('list-group-item');
-// console.log(items)
-console.log(items[0]);
-items[2].style.backgroundColor='green'
+// var itemall=document.getElementById('items');
+// console.log(itemall) 
+// itemall.style.fontWeight='bold'
+// var items=document.getElementsByClassName('list-group-item');
+// // console.log(items)
+// console.log(items[0]);
+// items[2].style.backgroundColor='green'
 
 
 // for(var i=0;i<items.length;i++){
@@ -14,7 +14,35 @@ items[2].style.backgroundColor='green'
 
 //  GETELEMENTBYTAGNAME //
 
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[0]);
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[0]);
+
+// QUERYSELECTER //
+//  var header =document.querySelector('#main-header');
+//  header.style.borderBottom ='solid 4px black'
+
+
+//  var input =document.querySelector('input');
+//  input.value='hello'
+
+//  var submit =document.querySelector('input[type="submit"]');
+//  submit.value='send'
+
+//  var item =document.querySelector('.list-group-item');
+//  item.style.color='red'
+
+//  var lastItem=document.querySelector('.list-group-item:last-child');
+//  lastItem.style.color='blue'
+
+ var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+ secondItem.style.backgroundColor='blue'
+
+ var secondItem=document.querySelector('.list-group-item:nth-child(3)');
+ secondItem.style.color='white'
+
+//  QUERYSELECTORALL //
+
+// var titles=document.querySelectorAll('.title');
+// console.log(titles)
 
